daf690a1dd92a5e455ded44097965df19485451b
Digoassun <digoassun@gmail.com>
2023-04-25 15:39:12 -0300

    Changing database config

--- src/config/database.js
+++ src/config/database.js
@@ -1,14 +1,28 @@
+// module.exports = {
+//   "username": "postgres",
+//   "password": "123",
+//   "database": "database_development",
+//   "host": "localhost",
+//   "port":"5432",
+//   "dialect": "postgres",
+//   "define":{
+//     "timestamps":true,
+//     "underscored":true,
+//     "undercoredAll":true
+//   }
+// }
+
 module.exports = {
   "username": "postgres",
-  "password": "123",
+  "password": "12345678",
   "database": "database_development",
-  "host": "localhost",
-  "port":"5432",
+  "host": "databasedevelopment.cb6klbla6ydr.sa-east-1.rds.amazonaws.com",
+  "port": "5432",
   "dialect": "postgres",
-  "define":{
-    "timestamps":true,
-    "underscored":true,
-    "undercoredAll":true
+  "loggin": false,
+  "define": {
+    "timestamps": true,
+    "underscored": true,
+    "undercoredAll": true
   }
 }
-
